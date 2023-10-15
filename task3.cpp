407d3ad34360866dfce9fc155dd22c3fb204983e
Iviyan <iviya.vv@gmail.com>
2023-10-15 19:41:30 +0300

    3 task completed

--- task3.cpp
+++ task3.cpp
@@ -5,19 +5,22 @@
 #include "utils.h"
 #include <windows.h>
 
+#define PRINTALL
+//#define TESTDATA
+
 using std::cout, std::cin, std::endl, std::vector;
 
 struct point {
     char name;
+    int index;
     int x;
     int y;
 
     point() = default;
-    point(char name, int x, int y) : name{name}, x{x}, y{y} {}
+    point(char name, int x, int y) : name{name}, x{x}, y{y}, index{0} {}
 };
 
-std::ostream& operator<<(std::ostream& os, const point& p)
-{
+std::ostream &operator<<(std::ostream &os, const point &p) {
     os << p.name << "(" << p.x << "," << p.y << ")";
     return os;
 }
@@ -27,8 +30,7 @@ struct route {
     double length = 0;
 };
 
-std::ostream& operator<<(std::ostream& os, const route& r)
-{
+std::ostream &operator<<(std::ostream &os, const route &r) {
     if (!r.path.empty())
         os << r.path[0];
     for (int i = 1; i < r.path.size(); i++)
@@ -51,46 +53,63 @@ int findIndex(const std::vector<T> &vec, Predicate pred) {
     return -1;
 }
 
-template<typename T, typename Predicate>
-bool find(const std::vector<T> &vec, Predicate pred, T &result) {
-    auto it = std::find_if(vec.begin(), vec.end(), pred);
-    if (it == vec.end()) return false;
-    result = *it;
-    return true;
+void hamiltonianPath(
+        vector<point> const &points, double const *const *m, size_t size,
+        vector<point> &path, route &shortestRoute);
+
+void hamiltonianCycle(
+        vector<point> const &points, double const *const *m, size_t size,
+        vector<point> &path, route &shortestRoute);
+
+route hamiltonianPath(vector<point> const &points, double const *const *m, size_t size) {
+    vector<point> path;
+    route route = {vector<point>(), 0xffffffff};
+    hamiltonianPath(points, m, size, path, route);
+    return route;
 }
 
-void hamiltonianPath(
-        vector<point> const &points, double **m, size_t size,
-        vector<point> &path, route &shortestRoute
-);
+route hamiltonianCycle(vector<point> const &points, double const *const *m, size_t size) {
+    vector<point> path;
+    route route = {vector<point>(), 0xffffffff};
+    hamiltonianCycle(points, m, size, path, route);
+    return route;
+}
 
 int main_() {
-    SetConsoleOutputCP( 65001 );
+    SetConsoleOutputCP(65001);
 
     vector<point> points;
 
     cout << "Введите список точек." << endl;
 
-    /*for (;;) {
+#ifndef TESTDATA
+    for (;;) {
         cout << "Введите имя точки из 1 символа (0 - завершить ввод), и её координаты x, y: ";
         point p = point();
         cin >> p.name;
         if (p.name == '0') break;
+        if (findIndex(points, [&](auto &item) { return item.name == p.name; }) != -1) {
+            cout << "Данное имя точки уже использовано!" << endl;
+            continue;
+        }
         cin >> p.x >> p.y;
         points.push_back(p);
-        // TODO: check repeating names
-    }*/
-
+    }
+#else
     points.emplace_back('a', 1, 2);
     points.emplace_back('b', 4, 3);
     points.emplace_back('c', 2, 3);
     points.emplace_back('d', 2, 5);
-    points.emplace_back('e', 3, 2);
+    /*points.emplace_back('e', 3, 2);
     points.emplace_back('f', 1, 4);
     points.emplace_back('j', 1, 4);
     points.emplace_back('k', 1, 4);
     points.emplace_back('l', 1, 4);
-    //points.emplace_back('n', 1, 4);
+    points.emplace_back('n', 1, 4);*/
+#endif
+
+    for (int i = 0; i < points.size(); i++)
+        points[i].index = i;
 
     size_t size = points.size();
 
@@ -113,11 +132,26 @@ int main_() {
     print_matrix(m, size, size);
     cout << endl;
 
-    vector<point> path;
-    route route = {vector<point>(), 0xffffffff};
-    hamiltonianPath(points, m, size, path, route);
+    {
+        route route = hamiltonianPath(points, m, size);
 
-    cout << "Результат" << endl << route << endl;
+#ifdef PRINTALL
+        cout << endl;
+#endif
+
+        cout << "Кратчайший гамильтонов путь: " << endl << route << endl << endl;
+    }
+
+    {
+        route route = hamiltonianCycle(points, m, size);
+
+
+#ifdef PRINTALL
+        cout << endl;
+#endif
+
+        cout << "Кратчайший гамильтонов цикл: " << endl << route << endl;
+    }
 
     for (int i = 0; i < size; i++)
         delete[] m[i];
@@ -127,17 +161,15 @@ int main_() {
 }
 
 void hamiltonianPath(
-        vector<point> const &points, double **m, size_t size,
+        vector<point> const &points, double const *const *m, size_t size,
         vector<point> &path, route &shortestRoute
 ) {
     if (path.size() < size) {
         for (int i = 0; i < size; i++) {
             point p = points[i];
-            if (std::any_of(path.begin(), path.end(), [&](const auto &item) {
-                return item.name == p.name;
-            })) {
+
+            if (findIndex(path, [&](auto &item) { return item.name == p.name; }) != -1)
                 continue;
-            }
 
             path.push_back(p);
             hamiltonianPath(points, m, size, path, shortestRoute);
@@ -148,15 +180,56 @@ void hamiltonianPath(
         for (int i = 0; i < size - 1; i++) {
             point &p1 = path[i];
             point &p2 = path[i + 1];
-            int p1Index = findIndex(points, [&](auto &item) { return item.name == p1.name; });
-            int p2Index = findIndex(points, [&](auto &item) { return item.name == p2.name; });
 
-            pathLength += m[p1Index][p2Index];
+            pathLength += m[p1.index][p2.index];
         }
+        pathLength = round(pathLength, 2);
 
-        //cout << route{path, pathLength} << endl;
+#ifdef PRINTALL
+        cout << route{path, pathLength} << endl;
+#endif
         if (pathLength < shortestRoute.length) {
             shortestRoute = {path, pathLength};
+        }
+    }
+}
+
+void hamiltonianCycle(
+        vector<point> const &points, double const *const *m, size_t size,
+        vector<point> &path, route &shortestRoute
+) {
+    if (path.size() < size) {
+        for (int i = 0; i < size; i++) {
+            point p = points[i];
+
+            if (findIndex(path, [&](auto &item) { return item.name == p.name; }) != -1)
+                continue;
+
+            path.push_back(p);
+            hamiltonianCycle(points, m, size, path, shortestRoute);
+            path.pop_back();
+        }
+    } else {
+        double pathLength = 0;
+        for (int i = 0; i < size; i++) {
+            point &p1 = path[i];
+            point &p2 = i == size - 1 ? path[0] : path[i + 1];
+
+            pathLength += m[p1.index][p2.index];
+        }
+        pathLength = round(pathLength, 2);
+
+#ifdef PRINTALL
+        {
+            vector<point> fullPath = path;
+            fullPath.push_back(path[0]);
+            cout << route{fullPath, pathLength} << endl;
+        }
+#endif
+        if (pathLength < shortestRoute.length) {
+            vector<point> fullPath = path;
+            fullPath.push_back(path[0]);
+            shortestRoute = {fullPath, pathLength};
         }
     }
 }
